1e1c3fe5d335e5e58a31a54ddca88b30c85fc38d
Dmitrij Vasiluk <vasilukwolf@gmail.com>
2016-08-03 23:32:43 +0300

    Add delete subimage jquery class

--- public/js/functions.js
+++ public/js/functions.js
@@ -59,12 +59,3 @@ $.ajax({
      }
   });
 });
-
-$('.add_images').click(function()
-{
-all=$('input[name="preview[]"]');
-if(all.length==11) return; //ограничим количество картинок 1 превью и 10 дополнительных картинок.
-field=$('input[name="preview[]"]:first').clone(); // клонируем поле preview
-$(this).after(field); //вставляем поле после кнопки
-
-});
